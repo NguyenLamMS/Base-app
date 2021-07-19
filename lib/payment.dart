60f0753fe91801e53ebf1d0bffb79c872c28bb93
Nguyen Lam <lam1997.ln@gmail.com>
2021-07-19 11:38:40 +0700

    update

--- lib/payment.dart
+++ lib/payment.dart
@@ -111,7 +111,11 @@ class _PaymentState extends State<Payment> {
                             productDetails: productDetails,
                             applicationUserName: null,
                           );
-                          _inAppPurchase.buyConsumable(purchaseParam: purchaseParam, autoConsume: true);
+                          if(productDetails.id.contains("donate")){
+                            _inAppPurchase.buyConsumable(purchaseParam: purchaseParam, autoConsume: false);
+                          }else{
+                            _inAppPurchase.buyNonConsumable(purchaseParam: purchaseParam);
+                          }
                         },
                       )),
           ),
