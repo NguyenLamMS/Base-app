7e707e26322d6682a7a0686291762133cc9b7feb
Nguyen Lam <lam1997.ln@gmail.com>
2021-08-14 10:57:33 +0700

    update

--- lib/app.dart
+++ lib/app.dart
@@ -8,6 +8,6 @@ class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return RandomNumber();
+    return Payment();
   }
 }
