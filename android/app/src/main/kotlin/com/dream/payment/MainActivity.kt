3b7dd977c85273e441b90bfba1560ca4dbf43c8a
Nguyen Lam <lamnv@itvnapp.dev>
2021-08-16 10:04:52 +0700

    update

--- android/app/src/main/kotlin/com/dream/payment/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.dream.payment
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/random/allpassword/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.random.allpassword
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
