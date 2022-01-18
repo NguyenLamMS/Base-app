708694299c18a5ab14efdd0b69d59bfd16365cc0
Nguyen Lam <lam1997.ln@gmail.com>
2022-01-18 12:44:56 +0700

    update

--- lib/home.dart
+++ lib/home.dart
@@ -1,9 +1,10 @@
+import 'dart:async';
 import 'dart:convert';
 import 'dart:math';
 import 'dart:ui';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_fortune_wheel/flutter_fortune_wheel.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:payment/diamond.dart';
@@ -17,120 +18,113 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  late List<dynamic> listData;
-  String quote = "Welcome";
-  String image ="image1.jpg";
-  Random random = Random();
-  List<String> listImage = ["image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg", "image5.jpg"];
+  StreamController<int> controller = StreamController<int>();
+  TextEditingController textEditingController = TextEditingController(text: "One, Two, Three");
+  List<String> listItem = <String>[].obs;
   @override
   void initState() {
     super.initState();
+    listItem.addAll(textEditingController.text.split(','));
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    textEditingController.dispose();
+    controller.close();
   }
   @override
   Widget build(BuildContext context) {
     final Diamond diamond = Get.put(Diamond.instance);
     return Scaffold(
-      body: Column(
-      children: [
-        Expanded(
-          child: Stack(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
             children: [
-              Positioned.fill(
-                  child: Image.asset(
-                "assets/images/" + image,
-                fit: BoxFit.cover,
-              )),
-              Positioned.fill(
-                  child: BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                child: Container(
-                  color: Colors.grey.withOpacity(0.05),
-                  alignment: Alignment.center,
-                  child: FutureBuilder<List<dynamic>>(
-                    future: readData(),
-                    builder: (context, snapshot) {
-                      if(snapshot.hasData){
-                       listData = snapshot.data!;
-                       quote = listData[random.nextInt(listData.length)];
-                        return Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                           quote,
-                            style: TextStyle(color: Colors.white, fontSize: 22, fontWeight: FontWeight.w800),
-                            textAlign: TextAlign.center,
-                          ),
-                        );
-                      }
-                      return Center(child: CircularProgressIndicator(color: Colors.deepOrangeAccent,));
-                    }
-                  ),
-                ),
-              )),
-              Positioned(
-                top: 0,
-                right: 0,
-                left: 0,
-                child: SafeArea(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        Text("Random Quotes", style: Theme.of(context).textTheme.headline6?.copyWith(color: Colors.white),),
-                        InkWell(
-                          onTap: (){
-                            Get.to(() => Payment(diamond: diamond,));
-                          },
-                          child: Row(
+              Container(
+                padding: EdgeInsets.all(8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Fortune Wheel', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),),
+                    InkWell(
+                      onTap: (){
+                        Get.to(() => Payment(diamond: diamond));
+                      },
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Row(
                             children: [
-                              FaIcon(FontAwesomeIcons.gem, size: 16, color: Colors.white,),
+                              FaIcon(FontAwesomeIcons.gem, size: 15,),
                               SizedBox(width: 4,),
-                              Obx(() => Text(diamond.diamondTotal.toString(), style: Theme.of(context).textTheme.button?.copyWith(color: Colors.white)))
+                              Obx(() => Text(diamond.diamondTotal.value.toString()))
                             ],
                           ),
-                        )
-                      ],
-                    ),
-                  ),
+                          Text('Buy Diamonds')
+                        ],
+                      ),
+                    )
+                  ],
                 ),
               ),
-            ],
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            children: [
-              Expanded(
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 24),
+                width: 350,
+                height: 350,
+                child: Obx(() => FortuneWheel(
+                    selected: controller.stream,
+                    animateFirst: false,
+                    indicators: [
+                      FortuneIndicator(
+                          alignment: Alignment.topCenter,
+                          child: TriangleIndicator(
+                            color: Colors.red,
+                          )
+                      )
+                    ],
+                    items: listItem.map((e) => FortuneItem(child: Text(e.toString()))).toList()
+                )),
+              ),
+              Container(
+                  margin: EdgeInsets.all(8),
+                  width: double.infinity,
+                  height: 200,
+                  child: Card(
+                    child: CupertinoTextField(
+                      controller: textEditingController,
+                      onChanged: (text){
+                        List<String> values = text.split(',');
+                        if(values.length > 1){
+                          listItem.clear();
+                          listItem.addAll(values);
+                        }
+                      },
+                    ),
+                  )
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
                 child: CupertinoButton(child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text("Random ("),
-                    FaIcon(FontAwesomeIcons.gem, size: 16, color: Colors.white,),
+                    Text("Turn ("),
+                    FaIcon(FontAwesomeIcons.gem, size: 15,),
                     SizedBox(width: 4,),
-                    Text("1)", style: Theme.of(context).textTheme.button?.copyWith(color: Colors.white))
+                    Text('1)')
                   ],
-                ),onPressed: () {
-                  if(diamond.diamondTotal.value > 0){
-                    setState(() {
-                      quote = listData[random.nextInt(listData.length)];
-                      image = listImage[random.nextInt(listImage.length)];
-                    });
-                  }
-                  diamond.subDiamond(1);
-                }, color: Colors.red),
-              ),
+                ), color: Colors.red, onPressed: (){
+                    if(diamond.diamondTotal.value > 0){
+                      controller.add(Random().nextInt(listItem.length + 1));
+                    }
+                    diamond.subDiamond(1);
+                }),
+              )
             ],
           ),
-        )
-      ],
-    ));
-  }
-  Future<List<dynamic>> readData() async {
-    String data = await DefaultAssetBundle.of(context).loadString("assets/data/data.json");
-    final jsonResult = json.decode(data);
-    List<dynamic> result = jsonResult["data"].toList();
-    return result;
+        ),
+      ),
+    );
   }
 }
