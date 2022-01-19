ded617f04b094435b386be42e518747327d170f6
Nguyen Lam <lam1997.ln@gmail.com>
2022-01-19 10:11:39 +0700

    update

--- lib/home.dart
+++ lib/home.dart
@@ -17,11 +17,6 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  late List<dynamic> listData;
-  String quote = "Welcome";
-  String image ="image1.jpg";
-  Random random = Random();
-  List<String> listImage = ["image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg", "image5.jpg"];
   @override
   void initState() {
     super.initState();
@@ -30,107 +25,22 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     final Diamond diamond = Get.put(Diamond.instance);
     return Scaffold(
-      body: Column(
-      children: [
-        Expanded(
-          child: Stack(
-            children: [
-              Positioned.fill(
-                  child: Image.asset(
-                "assets/images/" + image,
-                fit: BoxFit.cover,
-              )),
-              Positioned.fill(
-                  child: BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                child: Container(
-                  color: Colors.grey.withOpacity(0.05),
-                  alignment: Alignment.center,
-                  child: FutureBuilder<List<dynamic>>(
-                    future: readData(),
-                    builder: (context, snapshot) {
-                      if(snapshot.hasData){
-                       listData = snapshot.data!;
-                       quote = listData[random.nextInt(listData.length)];
-                        return Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                           quote,
-                            style: TextStyle(color: Colors.white, fontSize: 22, fontWeight: FontWeight.w800),
-                            textAlign: TextAlign.center,
-                          ),
-                        );
-                      }
-                      return Center(child: CircularProgressIndicator(color: Colors.deepOrangeAccent,));
-                    }
-                  ),
-                ),
-              )),
-              Positioned(
-                top: 0,
-                right: 0,
-                left: 0,
-                child: SafeArea(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        Text("Random Quotes", style: Theme.of(context).textTheme.headline6?.copyWith(color: Colors.white),),
-                        InkWell(
-                          onTap: (){
-                            Get.to(() => Payment(diamond: diamond,));
-                          },
-                          child: Row(
-                            children: [
-                              FaIcon(FontAwesomeIcons.gem, size: 16, color: Colors.white,),
-                              SizedBox(width: 4,),
-                              Obx(() => Text(diamond.diamondTotal.toString(), style: Theme.of(context).textTheme.button?.copyWith(color: Colors.white)))
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            children: [
-              Expanded(
-                child: CupertinoButton(child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text('Convert uppercase lowercase'),
+                Row(
                   children: [
-                    Text("Random ("),
-                    FaIcon(FontAwesomeIcons.gem, size: 16, color: Colors.white,),
-                    SizedBox(width: 4,),
-                    Text("1)", style: Theme.of(context).textTheme.button?.copyWith(color: Colors.white))
+
                   ],
-                ),onPressed: () {
-                  if(diamond.diamondTotal.value > 0){
-                    setState(() {
-                      quote = listData[random.nextInt(listData.length)];
-                      image = listImage[random.nextInt(listImage.length)];
-                    });
-                  }
-                  diamond.subDiamond(1);
-                }, color: Colors.red),
-              ),
-            ],
-          ),
-        )
-      ],
-    ));
-  }
-  Future<List<dynamic>> readData() async {
-    String data = await DefaultAssetBundle.of(context).loadString("assets/data/data.json");
-    final jsonResult = json.decode(data);
-    List<dynamic> result = jsonResult["data"].toList();
-    return result;
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
